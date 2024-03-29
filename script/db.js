784ec8f0ed3b60412996f485811c959753fec294
ArmVard <Vardanyan246@gamil.com>
2024-03-29 17:16:13 +0400

    my first repo

---
+++ script/db.js
@@ -0,0 +1,50 @@
+export const questions = [
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում1',
+        quiz: 'console.log(Hello World)',
+        options: [
+            'world',
+            'Hello World',
+            'hello',
+            'World Hello'
+        ],
+        correctOption: 1
+    },
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում2',
+        quiz: 'console.log(world)',
+        options: [
+            'world',
+            'Hello World',
+            'hello',
+            'World Hello'
+        ],
+        correctOption: 0
+    },
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում3',
+        quiz: 'console.log(worlddd)',
+        options: [
+            'worlddd',
+            'Hello World',
+            'hello',
+            'World Hello',
+            'World Hello'
+
+        ],
+        correctOption: 0
+    },
+    {
+        quizTitle: 'Ի՞նչ կտպվի console-ում4',
+        quiz: 'console.log(hello)',
+        options: [
+            'worlddd',
+            'Hello World',
+            'hello',
+            'World Hello',
+            'World Hello'
+
+        ],
+        correctOption: 3
+    },
+]
